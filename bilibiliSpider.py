a66ac7ebe89da29ff10553c4673b6ca570d89883
dhx20150812 <1793516813@qq.com>
2017-02-28 14:37:39 +0800

    Add files via upload

---
+++ bilibiliSpider.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+import requests
+import sys
+import re
+
+class BilibiliSpider:
+	def __init__(self,videoId):
+		self.videoUrl = 'http://www.bilibili.com/video/av' + str(videoId) + '/'
+		self.commentBaseUrl = 'http://comment.bilibili.com/'
+
+	def getCommentCid(self):
+		try:
+			html = requests.get(self.videoUrl).content
+		except:
+			print ('cannot open video pages')
+			sys.exit(0)
+		regex = re.compile('cid=(\d{7})')
+		try:
+			cid = re.findall(regex,html)[0]
+		except:
+			print ('cannot find cid!')
+			sys.exit(0)
+		return cid
+
+	def getComments(self):
+		cid = self.getCommentCid()
+		commentUrl = self.commentBaseUrl + str(cid) + '.xml'
+		try:
+			commentXml = requests.get(commentUrl).content
+		except:
+			print ('cannot open comment XML files')
+			sys.exit(0)
+		regex = re.compile('<d p=(.*?)>(.*?)</d>')
+		try:
+			comments = re.findall(regex,commentXml)
+		except:
+			print ('cannot finnd comments')
+			sys.exit(0)
+		return comments
+
+if __name__ == '__main__':
+	spider = BilibiliSpider(5043905)
+	comments = spider.getComments()
+	for comment in comments:
+		print(comment[1])
